19fdd73aae758c1fc513ce7823e7b7ff55cf3421
likildikil <likildikil@gmail.com>
2020-10-06 08:00:22 +0530

    DatePickerSwiftKSK

---
+++ DatePicker/ViewController.swift
@@ -0,0 +1,49 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dateField: UITextField!
+    
+    let picker = UIDatePicker()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        createDatePicker()
+    }
+    
+    func createDatePicker() {
+        
+        // toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // done button for toolbar
+        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([done], animated: false)
+        
+        dateField.inputAccessoryView = toolbar
+        dateField.inputView = picker
+        
+        // format picker for date
+   
+        picker.datePickerMode = .dateAndTime
+    }
+    
+    @objc func donePressed() {
+        // format date
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+          formatter.dateFormat = "MM/dd/yyyy hh:mm a"
+        let dateString = formatter.string(from: picker.date)
+        
+        dateField.text = "\(dateString)"
+        self.view.endEditing(true)
+    }
+
+}
+
